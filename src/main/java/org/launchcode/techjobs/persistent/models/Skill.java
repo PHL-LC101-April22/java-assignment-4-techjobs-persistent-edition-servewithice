46bf89847338feb3f2fa2346efa1ee03d8ba4454
arielmitchell <ariel.l.makowski@gmail.com>
2022-10-10 21:38:10 -0400

    almost done with task 4

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,8 +1,11 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -10,6 +13,9 @@ public class Skill extends AbstractEntity {
     @NotNull(message="Please add a location")
     @Size(min=3, message="Description must be at least 3 characters.")
     private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
 
     public Skill(String description) {
         this.description = description;
@@ -24,5 +30,13 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
